58a6bd515257dca90cb25d7b5be651d673fc098d
Forec <Forect@hotmail.com>
2016-10-12 13:42:49 +0800

    add example:

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# text-cooccurrence
-简单文本共现网络提取
+# Text Cooccurrence Example（文本共现网络提取示例）
+> This repository is just an example for the simplest text cooccurrence network capture, I wrote a tutorials at [here](#TODO).  
+I also wrote an article talking about basic coocurrence network capture at my [blog](https://forec.github.io/2016/10/03/co-occurrence-structure-capture/).  
+If you have any problems/ideas, please [contact me](mailto:forec@bupt.edu.cn), or open your PR. I feel honored to learn from your help.
+
+## Platform
+Written in `Python 2.7`, using `jieba`, `codecs`. You need to install `jieba` by `pip2 install jieba`.
+
+## Example
+This example is build from 《Train To Busan》, you can download the script from [here](http://7xktmz.com1.z0.glb.clouddn.com/Train%20to%20Busan.txt).   
+<img src="http://7xktmz.com1.z0.glb.clouddn.com/train-to-fusan.svg" width="400px"/>
+
+## Update-logs
+* 2016-10-12: Build repository
+
+# License
+All codes in this repository are licensed under the terms you may find in the file named "LICENSE" in this directory.
---
+++ example.py
@@ -0,0 +1,50 @@
+import os, sys
+import jieba, codecs, math
+import jieba.posseg as pseg
+import win32com.client
+
+
+names = {}
+relationships = {}
+lineNames = []
+
+sameNameSet = []
+
+# count names
+jieba.load_userdict("dict.txt")
+with codecs.open("to_train.txt", "r", "utf8") as f:
+	for line in f.readlines():
+		poss = pseg.cut(line)
+		lineNames.append([])
+		for w in poss:
+			if w.flag != "nr" or len(w.word) < 2:
+				continue
+			lineNames[-1].append(w.word)
+			if names.get(w.word) is None:
+				names[w.word] = 0
+				relationships[w.word] = {}
+			names[w.word] += 1
+
+# explore relationships
+for line in lineNames:
+	for name1 in line:
+		for name2 in line:
+			if name1 == name2:
+				continue
+			if relationships[name1].get(name2) is None:
+				relationships[name1][name2]= 1
+			else:
+				relationships[name1][name2] = relationships[name1][name2]+ 1
+
+# output
+with codecs.open("node.txt", "w", "gbk") as f:
+	f.write("Id Label Weight\r\n")
+	for name, times in names.items():
+		f.write(name + " " + name + " " + str(times) + "\r\n")
+
+with codecs.open("edge.txt", "w", "gbk") as f:
+	f.write("Source Target Weight\r\n")
+	for name, edges in relationships.items():
+		for v, w in edges.items():
+			if w > 10:
+			f.write(name + " " + v + " " + str(w) + "\r\n")
